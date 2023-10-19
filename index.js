dc5460b666b475c2269e09c7e2ff0ae41fb8d193
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-19 13:52:36 +0600

    set up  user name and passwrod

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// autoMobile
-// q3TAmPvvdxN4Ex4T
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktmmaha.mongodb.net/?retryWrites=true&w=majority`;
 
