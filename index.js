3c8ca72d39cb5ac69a171ada73d30f3615f0c2cb
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-19 21:07:44 +0600

    update operation setup

--- index.js
+++ index.js
@@ -14,54 +14,75 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const productCollection = client.db('productDB').collection('product');
+        const productCollection = client.db('productDB').collection('product');
 
-    app.get('/products', async(req, res) => {
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    app.post('/products', async(req, res)=>{
-        const newProducts = req.body;
-        console.log(newProducts);
-        const result = await productCollection.insertOne(newProducts)
-        res.send(result)
-    })
+        app.post('/products', async (req, res) => {
+            const newProducts = req.body;
+            console.log(newProducts);
+            const result = await productCollection.insertOne(newProducts)
+            res.send(result)
+        })
 
-    app.get('/products/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await productCollection.findOne(query);
-        res.send(result);
-    })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
 
-    app.delete('/products/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id : new ObjectId(id) }
-        const result = await productCollection.deleteOne(query);
-        res.send(result);
-    })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedProduct = req.body;
+            const products = {
+                $set: {
+                    name: updatedProduct.name,
+                    brand: updatedProduct.brand,
+                    price: updatedProduct.price,
+                    rating: updatedProduct.rating,
+                    details: updatedProduct.details,
+                    photo: updatedProduct.photo
+                }
+            }
+
+            const result = await productCollection.updateOne(filter, products, options);
+            res.send(result);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -70,6 +91,6 @@ app.get('/', (req, res) => {
     res.send('Automobile Web is running')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Automobile server is running on port : ${port}`)
 })
