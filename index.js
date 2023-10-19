8992241166423eb16c1d6d75ac3711306997e609
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-19 17:06:25 +0600

    create  update operation

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.findOne(query);
+        res.send(result);
+    })
+
     app.delete('/products/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id : new ObjectId(id) }
